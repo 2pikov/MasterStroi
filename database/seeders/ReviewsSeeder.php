62a6f5f2b07c3c50a18c112a56ee836ee5d8754d
2pikov <myaut184@gmail.com>
2025-06-21 02:55:51 +0400

    fix: correct date formatting in ReviewsSeeder

--- database/seeders/ReviewsSeeder.php
+++ database/seeders/ReviewsSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class ReviewsSeeder extends Seeder
 {
@@ -13,40 +14,43 @@ class ReviewsSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('reviews')->insert(
+        DB::table('reviews')->insert([
             [
-        [
-            'user_id'=>'2',
-            'product_id' => '1',
-            'rating' => '5',
-            'content' => ' Шпатлевка понравилась, разводится и ложится хорошо, высыхает как по инструкции, не слишком быстро. При высыхании становится белой.',
-            'is_approved'=>'1',
-            'created_at' => date('2025-05-19')
-        ],
-        [
-            'user_id'=>'5',
-            'product_id' => '1',
-            'rating' => '3',
-            'content' => 'Сыпится, непонравилась консистенция, цвет не белый получился, а сероватый',
-            'is_approved'=>'1',
-            'created_at' => date('2025-03-13')
-        ],
-        [
-            'user_id'=>'4',
-            'product_id' => '1',
-            'rating' => '4',
-            'content' => 'Отличная шпаклевка. Нигде не треснула, даже там где толстый слой. Грунтовкой не размывается, белого цвета. Хорошо шкурится.',
-            'is_approved'=>'1',
-            'created_at' => date('2025-03-13')
-        ],
-        [
-            'user_id'=>'6',
-            'product_id' => '2',
-            'rating' => '5',
-            'content' => 'Достойный товар, эластичность, мягкая, легка в нанесении',
-            'is_approved'=>'1',
-            'created_at' => date('2025-03-13')
-        ],
-    ]);
+                'user_id' => '2',
+                'product_id' => '1',
+                'rating' => '5',
+                'content' => ' Шпатлевка понравилась, разводится и ложится хорошо, высыхает как по инструкции, не слишком быстро. При высыхании становится белой.',
+                'is_approved' => '1',
+                'created_at' => Carbon::parse('2024-05-19 10:00:00'),
+                'updated_at' => Carbon::parse('2024-05-19 10:00:00'),
+            ],
+            [
+                'user_id' => '5',
+                'product_id' => '1',
+                'rating' => '3',
+                'content' => 'Сыпится, непонравилась консистенция, цвет не белый получился, а сероватый',
+                'is_approved' => '1',
+                'created_at' => Carbon::parse('2024-03-13 11:00:00'),
+                'updated_at' => Carbon::parse('2024-03-13 11:00:00'),
+            ],
+            [
+                'user_id' => '4',
+                'product_id' => '1',
+                'rating' => '4',
+                'content' => 'Отличная шпаклевка. Нигде не треснула, даже там где толстый слой. Грунтовкой не размывается, белого цвета. Хорошо шкурится.',
+                'is_approved' => '1',
+                'created_at' => Carbon::parse('2024-03-13 12:00:00'),
+                'updated_at' => Carbon::parse('2024-03-13 12:00:00'),
+            ],
+            [
+                'user_id' => '6',
+                'product_id' => '2',
+                'rating' => '5',
+                'content' => 'Достойный товар, эластичность, мягкая, легка в нанесении',
+                'is_approved' => '1',
+                'created_at' => Carbon::parse('2024-03-13 13:00:00'),
+                'updated_at' => Carbon::parse('2024-03-13 13:00:00'),
+            ],
+        ]);
     }
 }
